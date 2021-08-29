8681b12cd2a10773f679e27b4d72af74c9a11b3c
CS884335 <CS884335@wcupa.edu>
2021-08-29 18:56:55 -0400

    View for game

--- Tic-Tac-Toe/js/main.js
+++ Tic-Tac-Toe/js/main.js
@@ -1,10 +1,15 @@
 import Game from "./Game.js";
+import GameView from "./GameView.js";
 
 let game = new Game();
+let gameView = new GameView(document.getElementById("app"));
 
-console.log(game.turn);
-game.makeMove(0);
-console.log(game.board);
-console.log(game.turn);
-game.makeMove(6);
-console.log(game.board);
+gameView.onTileClick = function(i) {
+    console.log('Tile clicked: ${i}');
+};
+
+gameView.onRestartClick = function() {
+    console.log("Game is restarted!");
+};
+
+gameView.update(game);
